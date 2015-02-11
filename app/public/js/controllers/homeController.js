b28fb444b4b67de2ecc84bc79526a3017e393ca7
rr222cy <rr222cy@student.lnu.se>
2015-02-11 13:44:31 +0100

    Logout bug fixed

--- app/public/js/controllers/homeController.js
+++ app/public/js/controllers/homeController.js
@@ -39,21 +39,12 @@ function HomeController()
 			type: "POST",
 			data: {logout : true},
 			success: function(data){
-	 			that.showLockedAlert('You are now logged out.<br>Redirecting you back to the homepage.');
+	 			window.location.href = '/';
 			},
 			error: function(jqXHR){
 				console.log(jqXHR.responseText+' :: '+jqXHR.statusText);
 			}
 		});
-	}
-
-	this.showLockedAlert = function(msg){
-		$('.modal-alert').modal({ show : false, keyboard : false, backdrop : 'static' });
-		$('.modal-alert .modal-header h3').text('Success!');
-		$('.modal-alert .modal-body p').html(msg);
-		$('.modal-alert').modal('show');
-		$('.modal-alert button').click(function(){window.location.href = '/';})
-		setTimeout(function(){window.location.href = '/';}, 3000);
 	}
 }
 
